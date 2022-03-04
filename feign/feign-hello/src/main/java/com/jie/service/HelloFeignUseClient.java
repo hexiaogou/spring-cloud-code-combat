2a19cd62f73dc97efcc84d5c341a00d9ffe39cb2
何霖 <lin12.he@tcl.com>
2022-03-04 14:56:11 +0800

    clear unused package

--- feign/feign-hello/src/main/java/com/jie/service/HelloFeignUseClient.java
+++ feign/feign-hello/src/main/java/com/jie/service/HelloFeignUseClient.java
@@ -1,7 +1,6 @@
 package com.jie.service;
 
 import com.jie.config.HelloFeignServiceConfig;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
